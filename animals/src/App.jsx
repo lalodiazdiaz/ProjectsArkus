16d2899793780f5a7a0aadfa1fad3bbf552103b0
lalodiazdiaz <eduardodiazdiaz93@gmial.com>
2023-03-27 17:48:20 -0600

    states

---
+++ animals/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+function App() {
+const [count, setCount] = useState(0);
+    const handleClick=()=>{
+       setCount(count + 1)
+    }
+    return(
+        <div>
+            <button onClick={handleClick}>Add animal</button>
+            <div>
+                Number of animals:{count}
+            </div>
+        </div>
+    )
+}
+
+export default App;
