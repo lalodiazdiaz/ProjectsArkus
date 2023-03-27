89581b9e04d6d4607695d71dff60a017f3700dff
lalodiazdiaz <eduardodiazdiaz93@gmial.com>
2023-03-27 14:02:45 -0600

    new project

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,11 +1,24 @@
-function ProfileCard({ title, handle}) {
+function ProfileCard({ title, handle, image, description }) {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
 
-    return(
-    <div>
-        <div>Title is {title}</div>
-        <div>Handle is {handle}</div>
+      <div className="card-content">
+        <div className="media_content">
+          <p className='title is_4'>Title is {title}</p>
+          <p className='subtitle is_6'>Handle is {handle}</p>
+        </div>
+
+        <div className='content'>
+            {description}
+        </div>
+      </div>
     </div>
-    )
+  );
 }
 
-export default ProfileCard
+export default ProfileCard;
