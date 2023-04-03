acb7e115f70bad466c7115ae3c2c772bc227f65c
lalodiazdiaz <eduardodiazdiaz93@gmial.com>
2023-04-03 17:44:50 -0600

    useEffect

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import BookCreate from "./Components/BookCreate";
 import BookList from "./Components/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
 
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
   const editBookById = (id, newTitle) => {
     const updateBooks = books.map((book) => {
       if ((book.id = id)) {
@@ -24,11 +33,12 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updateBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title },
-    ];
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    const updateBooks = [...books, response.data];
     setBooks(updateBooks);
   };
   return (
