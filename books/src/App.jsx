8495eff27257725e0c608a2c1ce471b51b374b5c
lalodiazdiaz <eduardodiazdiaz93@gmial.com>
2023-04-03 15:03:44 -0600

    json server

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import BookCreate from "./Components/BookCreate";
+import BookList from "./Components/BookList";
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const editBookById = (id, newTitle) => {
+    const updateBooks = books.map((book) => {
+      if ((book.id = id)) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+
+    setBooks(updateBooks);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const createBook = (title) => {
+    const updateBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ];
+    setBooks(updateBooks);
+  };
   return (
-    <div>App</div>
-  )
+    <div className="app">
+      <h1>Reading list</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
-export default App
+export default App;
