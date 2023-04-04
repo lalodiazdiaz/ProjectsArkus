1fbc392ba3ac6f2529f232e72044fe94351ffedc
lalodiazdiaz <eduardodiazdiaz93@gmial.com>
2023-04-04 17:44:16 -0600

    finished lession

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -1,51 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import BookCreate from "./Components/BookCreate";
 import BookList from "./Components/BookList";
-import axios from "axios";
+import useBooksContext from "./hooks/useBooksContext";
 
 function App() {
-  const [books, setBooks] = useState([]);
-
-  const fetchBooks = async () => {
-    const response = await axios.get("http://localhost:3001/books");
-    setBooks(response.data);
-  };
+  const { fetchBooks } = useBooksContext();
 
   useEffect(() => {
     fetchBooks();
   }, []);
-  const editBookById = (id, newTitle) => {
-    const updateBooks = books.map((book) => {
-      if ((book.id = id)) {
-        return { ...book, title: newTitle };
-      }
-      return book;
-    });
 
-    setBooks(updateBooks);
-  };
-
-  const deleteBookById = (id) => {
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-
-    setBooks(updatedBooks);
-  };
-
-  const createBook = async (title) => {
-    const response = await axios.post("http://localhost:3001/books", {
-      title,
-    });
-
-    const updateBooks = [...books, response.data];
-    setBooks(updateBooks);
-  };
   return (
     <div className="app">
       <h1>Reading list</h1>
-      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
-      <BookCreate onCreate={createBook} />
+      <BookList />
+      <BookCreate />
     </div>
   );
 }
--- books/src/Components/BookList.jsx
+++ books/src/Components/BookList.jsx
@@ -1,10 +1,10 @@
+import useBooksContext from "../hooks/useBooksContext";
 import BookShow from "./BookShow";
 
-function BookList({ books, onDelete, onEdit }) {
+function BookList() {
+  const { books } = useBooksContext();
   const renderedBooks = books.map((book) => {
-    return (
-      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
-    );
+    return <BookShow key={book.id} book={book} />;
   });
   return <div className="book-list">{renderedBooks}</div>;
 }
